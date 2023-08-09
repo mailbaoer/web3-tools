55e497e25ef97e92b940c129269841cd8a57bd71
baoer <mailbaoer@gmail.com>
2023-08-09 22:24:45 +0800

    minor

--- src/components/BackgroundRemoval.vue
+++ src/components/BackgroundRemoval.vue
@@ -28,11 +28,11 @@ watch(() => file.value, async () => {
   <div class='flex flex-col items-center justify-center p-4'>
     <h3 class='my-6 text-2xl font-bold'>Remove Image Background</h3>
     <div class='flex space-x-6 h-full'>
-      <img :src='originalImage' width='300' height='auto' class='min-h-16' alt='original image' />
+      <img v-if="originalImage" :src='originalImage' width='300' height='auto' class='min-h-16' alt='original image' />
       <div v-if='isLoading' class='w-[300px] h-full flex items-center justify-center'>
         <span class='loading loading-ring loading-lg w-100 h-100 bg-gray-200 z-10'></span>
       </div>
-      <img v-else :src='imageURL' width='300' height='auto' class='min-h-16' alt='after image' />
+      <img v-if='imageURL' :src='imageURL' width='300' height='auto' class='min-h-16' alt='after image' />
     </div>
     
     <input type='file' :disabled='isLoading' class='file-input file-input-bordered w-full h-full my-4 max-w-xs'
